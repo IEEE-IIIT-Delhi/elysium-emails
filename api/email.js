cdb008531bd4f3e134873d778213b0ea87a38f47
Mihir Chaturvedi <plibither8@gmail.com>
2020-09-07 22:58:19 +0530

    init

---
+++ api/email.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const { Octokit } = require('@octokit/rest')
+const { validate } = require('email-validator')
+
+const { GIST_ID, GH_TOKEN } = process.env
+
+const octokit = new Octokit({ auth: `token ${GH_TOKEN}` }) // Instantiate Octokit
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function handler (req, res) {
+  const { email } = req.body
+  if (!validate(email)) {
+    return res.end({ success: false })
+  }
+  const gist = await octokit.gists.get({ gist_id: GIST_ID })
+  const emails = JSON.parse(gist.data.files['emails.json'].content)
+  if (email in emails) {
+    return res.end({ success: false })
+  }
+  emails.push(email)
+  console.log({email, emails})
+  await octokit.gists.update({
+    gist_id: GIST_ID,
+    files: {
+      'emails.json': {
+        content: JSON.stringify(emails, null, '  ')
+      }
+    }
+  })
+  return res.end({ success: true })
+}
+
+module.exports = allowCors(handler)
